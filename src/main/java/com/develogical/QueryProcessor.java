c57a403617d7f164ed669f36836d6cab7cf63209
dollin <neil.dolling@gmail.com>
2020-06-24 10:31:04 +0100

    largest exception fix and sum

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -8,9 +8,14 @@ public class QueryProcessor {
         if (query.toLowerCase().contains("romeo and juliet")) {
             return "William Shakespeare";
         }
+
         if (query.toLowerCase().contains("largest")) {
-            String[] numbers = query.substring(query.indexOf("largest:") + 10).replaceAll("%20", "").split(",");
+            String[] numbers = query.substring(query.indexOf("largest:") + 8).replaceAll("%20", "").split(",");
             return "" + Arrays.stream(numbers).mapToLong(Long::parseLong).max().getAsLong();
+        }
+        if (query.toLowerCase().contains("plus")) {
+            String[] numbers = query.substring(query.indexOf(":%20what%20is") + 15).replaceAll("%20", "").split("plus");
+            return "" + Arrays.stream(numbers).mapToLong(Long::parseLong).sum();
         }
         return "";
     }
--- src/test/java/com/develogical/QueryProcessorTest.java
+++ src/test/java/com/develogical/QueryProcessorTest.java
@@ -30,4 +30,14 @@ public class QueryProcessorTest {
   public void largest() {
     Assert.assertEquals(queryProcessor.process("5f6da380:%20which%20of%20the%20following%20numbers%20is%20the%20largest:%20387,%2098"), "387");
   }
+
+  @Test
+  public void largest1() {
+    Assert.assertEquals(queryProcessor.process("8611b2a0:%20which%20of%20the%20following%20numbers%20is%20the%20largest:%207,%20726"), "726");
+  }
+  @Test
+  public void plus() {
+    Assert.assertEquals(queryProcessor.process("362f1550:%20what%20is%208%20plus%203"), "11");
+  }
+
 }
