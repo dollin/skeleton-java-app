4ba4cc8d79996626c17a3778eb924388df5b5259
dollin <neil.dolling@gmail.com>
2020-06-24 10:42:21 +0100

    trim

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -11,15 +11,15 @@ public class QueryProcessor {
 
         if (query.toLowerCase().contains("largest")) {
             String[] numbers = query.substring(query.indexOf("largest:") + 8).replaceAll("%20", "").split(",");
-            return "" + Arrays.stream(numbers).mapToLong(Long::parseLong).max().getAsLong();
+            return "" + Arrays.stream(numbers).map(String::trim).mapToLong(Long::parseLong).max().getAsLong();
         }
         if (query.toLowerCase().contains("plus")) {
             String[] numbers = query.substring(query.indexOf(":%20what%20is") + 15).replaceAll("%20", "").split("plus");
-            return "" + Arrays.stream(numbers).mapToLong(Long::parseLong).sum();
+            return "" + Arrays.stream(numbers).map(String::trim).mapToLong(Long::parseLong).sum();
         }
         if (query.toLowerCase().contains("multiplied")) {
             String[] numbers = query.substring(query.indexOf(":%20what%20is") + 15).replaceAll("%20", "").split("multipliedby");
-            return "" + Long.valueOf(numbers[0]) * Long.valueOf(numbers[1]);
+            return "" + Long.valueOf(numbers[0].trim()) * Long.valueOf(numbers[1].trim());
         }
         return "";
     }
